a7621eb26253deeee2163060df3d3e6daa25c912
Gary Moore <Gary.Moore1@ibm.com>
2023-07-17 18:36:40 +0100

    create me oull initial neovim config 🔥

---
+++ init.lua
@@ -0,0 +1,39 @@
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.autoindent = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.o.termguicolors = true
+vim.o.background = 'dark'
+vim.cmd[[colorscheme gruvbox]]
+
+vim.cmd 'filetype plugin indent on'
+vim.cmd 'syntax on'
+
+vim.cmd [[packadd packer.nvim]]
+vim.cmd 'autocmd BufWritePost plugins.lua PackerCompile'
+
+require('plugins')
+
+require('mason').setup()
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = { "c", "vim", "typescript", "vimdoc", "javascript", "json" },
+  sync_install = false,
+  ignore_install = { "lua" },
+  highlight = {
+    enable = true,
+    disable = function(lang, buf)
+      local max_filesize = 100 * 1024 -- 100KB
+      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      if ok and stats and stats.size > max_filesize then
+        return true
+      end
+    end,
+    additional_vim_regex_highlighting = false,
+  },
+}
+
---
+++ lua/plugins.lua
@@ -0,0 +1,24 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.lua
+
+-- Only required if you have packer configured as `opt`
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+
+  use { "ellisonleao/gruvbox.nvim" }
+
+  use {
+    'williamboman/mason.nvim',
+    run = ':MasonUpdate' -- :MasonUpdate updates registry contents
+  }
+end)
+
